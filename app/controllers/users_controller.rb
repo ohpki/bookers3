0685196128efd534a65dbb602ee55257f0262b21
EC2 Default User <ohpkiki@gmail.com>
2022-03-09 07:25:26 +0000

    Initial Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+devise_for :users
+resources :books, only: [:index, :show, :edit, :create,:update, :destroy]
+resources :users, only: [:index, :show, :edit, :create,:update, :destroy]
+root :to => 'homes#top'
+get 'homes/about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
