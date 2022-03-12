504e84ae45c68a143c6e367f6ebf6c286889a326
EC2 Default User <ohpkiki@gmail.com>
2022-03-12 10:32:40 +0000

    add follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,28 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
   has_many :book_comments, dependent: :destroy
-  
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+
+
 
 def get_profile_image
   (profile_image.attached?) ? profile_image : 'no_image.jpg'
 end
 
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
 
 devise_for :users
 resources :books, only: [:index, :show, :edit, :create,:update, :destroy] do
-  resources:book_comments, only: [:create, :destroy]
-  resource:favorites, only: [:create, :destroy]
+
+  resources :book_comments, only: [:create, :destroy]
+  resource :favorites, only: [:create, :destroy]
 
 end
-resources :users, only: [:index, :show, :edit, :create,:update, :destroy]
+resources :users, only: [:index, :show, :edit, :create,:update, :destroy] do
+  resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+end
 root :to => 'homes#top'
 get 'homes/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
